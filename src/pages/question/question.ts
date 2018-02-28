a9a46eef25b312475cc242e3728fef60209721b1
oxosolutions <radhasingh226@gmail.com>
2018-02-28 18:03:02 +0530

    add previous button

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -18,6 +18,8 @@ export class QuestionPage {
 	questionTitle:any;
 	id:any;
 	questions=[];
+  previousButton:any;
+  NextButton:any;
   constructor(public alertCtrl: AlertController,public servicesProvider:AioneServicesProvider,public navCtrl: NavController, public navParams: NavParams) {
   }
   showConfirm() {
@@ -52,35 +54,64 @@ export class QuestionPage {
   ionViewDidLoad() {
     let single;
     let i=0;
+
     this.questionTitle=localStorage.getItem("ApplicationName");
     this.id=this.navParams.get('id');
     this.servicesProvider.SelectWhere("questions","group_id",this.id).then((result:any)=>{
       this.questions.push(result.rows);
-       this.textData(this.questions,i).then(()=>{
-
-       })                                                                                                                                                   
+        this.textData(this.questions,i).then(()=>{
+         console.log(this.questions);
+        })                                                                                                                                                   
     })
   }
   textData(questions,i){
+    console.log(questions[0]);
     return new Promise((resolve,reject)=>{
        questions.forEach((value,key)=>{
-        console.log(value[i]);
-        this.OriginalContent=value[i];
+          console.log(value[i]);
+          this.OriginalContent=value[i];
+          console.log(this.OriginalContent.idss);
+          
+          if(this.OriginalContent.idss==1 ){
+            this.previousButton=false;
+          }else{
+            this.previousButton=true;
+          }
+          // let hh=questions[0].length;
+          // console.log(hh);
+          // console.log(i);
+          // if(questions[0].length == i ){
+          //   this.NextButton=false;
+          // }else{
+          //   this.previousButton=true;
+          // }
         }); 
+
     })
       
       
   }
   next(id){
     let questionLength;
-    console.log(id);
-    console.log(this.questions);
     questionLength=this.questions[0].length;
-    console.log();
     if(id==questionLength){
       console.log("there is no data");
       this.navCtrl.setRoot(DashboardPage);
     }else{
+      console.log(id);
+      this.textData(this.questions,id).then(()=>{
+      });   
+    }
+  }
+  previous(id){
+    id=id-2;
+    let questionLength;
+    questionLength=this.questions[0].length;
+    if(id==questionLength){
+      console.log("there is no data");
+      this.navCtrl.setRoot(DashboardPage);
+    }else{
+      console.log(id);
       this.textData(this.questions,id).then(()=>{
       });   
     }
