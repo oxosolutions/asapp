964cf6b07cda8f1ee53623b631e10a5324c1b6e5
oxosolutions <radhasingh226@gmail.com>
2018-04-25 18:07:09 +0530

    resest password

--- src/pages/change-password/change-password.ts
+++ src/pages/change-password/change-password.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
-
+import {Validators, FormBuilder, FormGroup, NgForm, FormControl} from '@angular/forms';
+import {ToastController , LoadingController} from 'ionic-angular';
+import { Http, Headers, RequestOptions} from '@angular/http';
 /**
  * Generated class for the ChangePasswordPage page.
  *
@@ -14,15 +16,84 @@ import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angula
   templateUrl: 'change-password.html',
 })
 export class ChangePasswordPage {
+  newPassword : FormGroup;
+  result:any;
+   submitAttempt: boolean = false;
+  constructor(public toastctrl:ToastController, public loaderctrl:LoadingController,public http: Http,public fb: FormBuilder,public viewCtrl: ViewController,public navCtrl: NavController, public navParams: NavParams) {
+  }
 
-  constructor(public viewCtrl: ViewController,public navCtrl: NavController, public navParams: NavParams) {
+  
+  dismiss(){
+  	this.viewCtrl.dismiss();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ChangePasswordPage');
+    this.result=this.navParams.get("reset");
+    console.log(this.result);
+  
   }
-  dismiss(){
-  	this.viewCtrl.dismiss();
+   ionViewWillEnter(){
+    this.newPassword=this.fb.group({
+      Oldpassword:[ null, Validators.compose([  
+              Validators.required ,
+
+        ])],
+       newpass:[ null, Validators.compose([  
+              Validators.required ,
+
+        ])],
+        confirmpassword:[ null, Validators.compose([  
+              Validators.required ,
+
+        ])],
+       // email:[ null, Validators.compose([  
+       //        Validators.required,  
+       //        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+       //  ])],
+
+    })
+    
+  }
+  resetPassword(username){
+    this.submitAttempt = true;
+    console.log(username);
+    if(!this.newPassword.valid){
+        console.log('not valid');
+        this.newPassword;
+    }else{
+      console.log(this.newPassword.value);
+      //this.submit(this.loginUser.value.name,this.loginUser.value.email);    
+    }
+  }
+
+  submit(name,Email){
+    let loader =this.loaderctrl.create({
+      content:'<div class="custom-spinner-container"><div class="custom-spinner-box"></div>Submitting your Enquiry</div>',
+    });
+      loader.present();
+      let toast=this.toastctrl.create({
+        message:'Your Enquiry is Submitted',
+        duration:4000,
+        position:'top',
+      });
+      let form = new FormData();
+      // form.append('org_id','175');
+      form.append('name',name);
+      form.append('mobile',Email);
+      this.http.post("http://admin.scolm.com/api/send_complaint", form)
+      .subscribe(data => {
+        console.log(data);
+        this.newPassword.reset()
+         loader.dismiss();
+        toast.present();
+        console.log('submitted successfully');
+        
+      },error=>{
+        console.log(error);
+      });
+      // showalert(data);
+      return false;
+
   }
 
 }
