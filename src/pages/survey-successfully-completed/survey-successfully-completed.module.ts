94a1e44292fa0a0346f5e32e0457135f36d6a66c
ashish1155 <user1155995@gmail.com>
2018-05-04 15:25:32 +0530

    survey-success page design accomplished

---
+++ src/pages/survey-successfully-completed/survey-successfully-completed.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SurveySuccessfullyCompletedPage } from './survey-successfully-completed';
+
+@NgModule({
+  declarations: [
+    SurveySuccessfullyCompletedPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SurveySuccessfullyCompletedPage),
+  ],
+})
+export class SurveySuccessfullyCompletedPageModule {}
