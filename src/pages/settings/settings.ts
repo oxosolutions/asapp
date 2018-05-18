ee97f9f168f1d5047a8d5787816eed3632259d0c
oxosolutions <radhasingh226@gmail.com>
2018-05-18 12:03:00 +0530

    changes

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,27 +2,18 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DashboardPage } from '../../pages/dashboard/dashboard';
 
-
 @IonicPage()
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
-	hero=['ram','sham','rama'];
-	searchValue = '';
+	
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
-  addAll(event){
-  	this.searchValue=event;
-  	this.hero.push(this.searchValue);
-  	this.searchValue='';
-  	console.log(event);
-  	console.log(this.hero);
-  	
-  }
+  
 
 }
